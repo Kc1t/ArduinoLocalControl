8bda30fbb80d01b4556b8136782fc37d72f6c9fc
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2023-03-19 11:09:05 -0300

    led status

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,21 @@
-const ligarBtn = document.querySelector("#ligar");
-const desligarBtn = document.querySelector("#desligar");
+const ligarBtn = document.querySelector("#ligar"), 
+desligarBtn = document.querySelector("#desligar"),
+statusBtn = document.querySelector("#circle");
 
 ligarBtn.addEventListener("click", function() {
+
+  setTimeout(function() {
+
   fetch("/ligar", {
     method: "POST",
     body: "1",
   });
+  
+  statusBtn.style.background = "#F32C38";
+  statusBtn.style.boxShadow = "0px 0px 10px #F32C38";
+
+  }, 3000);
+
 });
 
 desligarBtn.addEventListener("click", function() {
@@ -13,4 +23,7 @@ desligarBtn.addEventListener("click", function() {
     method: "POST",
     body: "0",
   });
+
+  statusBtn.style.background = "#26313F";
+  statusBtn.style.boxShadow = "0px 0px 10px #26313F";
 });
